8f6d7c6f8d494f9fba9cf3046606bcdb95ca35da
Rob Landry <rob.a.landry@gmail.com>
2019-09-25 20:27:35 -0400

    Working module.

--- pywemo/ouimeaux_device/crockpot.py
+++ pywemo/ouimeaux_device/crockpot.py
@@ -27,6 +27,7 @@ else:
 # and the numbers have meaning.
 class CrockPotMode(IntEnum):
     """Enum to map WeMo modes to human-readable strings."""
+
     Off = 0
     Warm = 50
     Low = 51
@@ -41,21 +42,13 @@ MODE_NAMES = {
 }
 
 
-def attribute_xml_to_dict(xml_blob):
-    """Return integer value of Mode from an attributesList blob, if present."""
-    xml_blob = "<attributes>" + xml_blob + "</attributes>"
-    xml_blob = xml_blob.replace("&gt;", ">")
-    xml_blob = xml_blob.replace("&lt;", "<")
+def attribute_values_to_int(attributes):
+    """Return integer value of attributes."""
     result = {}
-    attributes = et.fromstring(xml_blob)
     for attribute in attributes:
-        # The coffee maker might also send unrelated xml blobs, e.g.:
-        # <ruleID>coffee-brewed</ruleID>
-        # <ruleMsg><![CDATA[Coffee's ready!]]></ruleMsg>
-        # so be sure to check the length of attribute
         if len(attribute) >= 2:
             try:
-                result[attribute[0].text] = int(attribute[1].text)
+                result[attribute] = int(attributes[attribute])
             except ValueError:
                 pass
     return result
@@ -71,20 +64,22 @@ class CrockPot(Switch):
 
     def __repr__(self):
         """Return a string representation of the device."""
-        return '<WeMo CrockPot "{name}">'.format(name=self.name)
+        return '<WeMo "{name}">'.format(name=self.name)
 
     def update_attributes(self):
         """Request state from device."""
         # pylint: disable=maybe-no-member
-        resp = self.basicevent.GetCrockpotState().get("attributeList")
-        LOG.critical("Resp: %s" % resp)
-        self._attributes = attribute_xml_to_dict(resp)
+        resp = self.basicevent.GetCrockpotState()
+        self._attributes = attribute_values_to_int(resp)
         self._state = self.mode
 
     def subscription_update(self, _type, _params):
         """Handle reports from device."""
+        LOG.debug("subscription_update %s %s", _type, _params)
+
+        # TODO: This most likely does nothing as attributeList doesnt exist
         if _type == "attributeList":
-            self._attributes.update(attribute_xml_to_dict(_params))
+            self._attributes.update(attribute_values_to_int(_params))
             self._state = self.mode
             return True
 
@@ -98,12 +93,17 @@ class CrockPot(Switch):
     @property
     def mode(self):
         """Return the mode of the device."""
-        return self._attributes.get("Mode")
+        return self._attributes.get("mode")
 
     @property
     def mode_string(self):
         """Return the mode of the device as a string."""
         return MODE_NAMES.get(self.mode, "Unknown")
+
+    @property
+    def attributes(self):
+        """Return the mode of the device."""
+        return self._attributes
 
     def get_state(self, force_update=False):
         """Return 0 if off and 1 if on."""
@@ -115,25 +115,26 @@ class CrockPot(Switch):
         # Consider the CrockPot to be "on" if it's not off.
         return int(self._state != CrockPotMode.Off)
 
-    def set_state(self, state):
+    def set_state(self, state, time=0):
         """
         Set the mode of this device (as int index of the Mode IntEnum).
 
         Provided for compatibility with the Switch base class.
         """
-        self.set_mode(state)
+        self.set_mode(state, time)
 
-    def set_mode(self, mode):
+    def set_mode(self, mode_state, time_state):
         """
         Set the mode of this device (as int index of the Mode IntEnum).
 
         Provided for compatibility with the Switch base class.
         """
+        # Make sure we have a value for time to send to device.
+        if not time_state:
+            time_state = 0
         # Send the attribute list to the device
         # pylint: disable=maybe-no-member
-        self.basicevent.SetAttributes(attributeList=quote_xml(
-            "<attribute><name>Mode</name><value>" +
-            str(int(mode)) + "</value></attribute>"))
+        self.basicevent.SetCrockpotState(mode=int(mode_state),time=int(time_state))
 
         # Refresh the device state
         self.get_state(True)
